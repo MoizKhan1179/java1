f61b9b33f48094ef46d95a995677cca63f63c5b8
MoizKhan1179 <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-14 18:59:05 +0500

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,280 @@
+// let percentage= prompt("enter your marks");
+
+// if(percentage >= 70 && percentage <= 100){
+
+// if(percentage >= 90){
+//     document.write("A+")
+// }else if(percentage >=80){
+//     document.write("Your grade is A1")
+// }else if(percentage >=70){
+//     document.write("Your grade is A")}
+// }
+// else if(percentage >= 60 && percentage <= 100){
+//     document.write("Your grade is B")
+// }
+// else if(percentage >= 50 && percentage <= 100){
+//     document.write("Your grade is C")
+// }
+// else if(percentage >= 40 && percentage <= 100){
+//     document.write("Your grade is D")
+// }
+// else if(percentage <= 40 && percentage <= 100){
+//     document.write("Your grade is F")
+// }
+
+
+//switch statement
+
+
+
+
+
+// let day= prompt("Enter number of days");
+
+
+// switch (+day) {
+//     case 1:
+//         document.write("Sunday")
+//         break;
+//     case 2:
+//         document.write("Monday")
+//         break;
+//     case 3:
+//         document.write("Tuesday")
+//         break;
+//     case 4:
+//         document.write("Wednesday")
+//         break;
+//     case 5:
+//         document.write("Thursday")
+//         break;
+//     case 6:
+//         document.write("Friday")
+//         break;
+//     case 7:
+//         document.write("Satursday")
+//         break;
+//         default:
+//             document.write("Please enter number from 1-7")
+// }
+
+
+
+                    // PERCENTAGE USING IF AND SWITCH
+
+// let percentage= prompt("enter your marks");
+
+// let result;
+
+// if(percentage >= 70 && percentage <= 100){
+
+// if(percentage >= 90){
+//     result = "A+"
+//     document.write("Your grade is " + result)
+// }else if(percentage >=80){
+//     result = "A"
+//     document.write("Your grade is " + result)
+// }else if(percentage >=70){
+//     result = "A"
+//     document.write("Your grade is " + result)}
+// }
+// else if(percentage >= 60 && percentage <= 70){
+//     result = "B"
+//     document.write("Your grade is " + result)
+// }
+// else if(percentage >= 50 && percentage <= 70){
+//     result = "C"
+//     document.write("Your grade is " + result)
+// }
+// else if(percentage >= 40 && percentage <= 70){
+//     result = "D"
+//     document.write("Your grade is " + result)
+// }
+// else if(percentage <= 40 && percentage >=0){
+//     result = "F"
+//     document.write("Your grade is " + result)
+// }
+
+// switch (result) {
+//     case "A+":
+//         document.write(" Pass")
+//         break;
+    
+//     case "A1":
+//         document.write (" Pass")
+//         break;
+//     case "A":
+//         document.write(" Pass")
+//         break;
+//     case "B":
+//         document.write(" Pass")
+//         break;
+//     case "C":
+//         document.write(" Pass")
+//         break;
+//     case "D":
+//         document.write(" Pass")
+//         break;
+//     case "F":
+//         document.write(" Fail")
+//         break;
+
+//     default: 
+//     document.write(" Please enter correct numbers")
+//            break; 
+// }
+
+                            // ******END*******
+
+
+
+
+
+                            //*****LOGIN SCENARIO******
+
+
+
+
+// let userName = prompt("Please Enter Your UserName")
+
+// // let user;
+// // // user="Abdul Moiz"
+
+// let email = prompt("Please Enter Your Email")
+// // email="@gmail.com"
+
+
+// let password = prompt("Please Enter Your Password")
+
+
+// // let pin = prompt("Please Enter Your Pin")
+// // let pinn;
+// // pin= 1179
+
+// // let pass;
+// // // pass="Shahid"
+
+
+// if (userName === "Abdul Moiz" && (password === "Shahid" || pin ==="1179")){
+//     console.log("You have successfuly Logged in")
+// }else{
+//     console.log("Invalid Username")
+// }
+
+// while loop
+
+// let a = 0;
+
+// while(a <= 10){
+
+//     if(a % 2 === 1){
+//         document.write(a)
+//     }
+    
+//     // document.write(-a);
+//     document.write("<br>")
+//     a++;
+// }
+
+// let b = 20;
+
+// while(a >= 1){
+
+//     // if(a % 2 === 1){
+//     //     document.write(a)
+//     // }
+    
+//     document.write(b);
+//     document.write("<br>")
+//     b++;
+// }
+
+// ************DO WHILE LOOP**********
+
+// let a=0
+ 
+// do{
+//     document.write(a + "<br>")
+// }
+// while(a <= 10)
+
+// *****TERNARY OPERATOR********
+
+// let age = prompt("Enter Your Age")
+
+// age <=18 ? document.write("Dont get a job") : document.write("Get a job")
+
+
+
+// let userName = prompt("Please Enter Your UserName")
+
+// // // user="Abdul Moiz"
+
+// let email = prompt("Please Enter Your Email")
+// // email="@gmail.com"
+
+
+// let password = prompt("Please Enter Your Password")
+
+
+// // let pin = prompt("Please Enter Your Pin")
+// // pin= 1179
+
+// // let pass;
+// // // pass="Shahid"
+
+
+// if ((userName === "Abdul Moiz"  || email === "@gmail.com") && password === "Shahid"){
+//     console.log("You have successfuly Logged in")
+// }else{
+//     console.log("Invalid Username or Password")
+// }
+
+
+
+//******** FOR LOOP **********
+
+// for(let d = 0; d <=10; d++){
+
+//     document.write(d + "<br>")
+// }
+
+// for (i =1 ; i<=100; i+=10){
+    
+//     for (j=i ; j<i+10 ; j++){
+//         document.write(`${j} &nbsp`)
+        
+//     }
+//     document.write(  `<br>`)
+
+// }
+
+
+// for(i=10; i>=1; i--){
+//     for(j=1; j<=i ; j++){
+//         document.write(i)
+//     }
+//     document.write(` <br>`)
+// }
+
+// for(k=1; k<=5; k++){
+//     for(l=1; l<=i ; l++){
+//         document.write(k)
+//     }
+//     document.write(` <br>`)
+// }
+
+
+// for(i=1 ; i<=10 ; i++){
+//     if(i<5){
+//         document.write("HELLO")
+//     }else(document.write("WORLD"))
+//     document.write(`${i} <br>`)
+// }
+
+// for(i=1 ; i<=10 ; i++){
+//     if(i<=5){
+//         document.write("okk")
+//     }
+//     document.write(`${i} <br>`)
+// }
